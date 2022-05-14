876b2cb4065110c57f12767b3bc5577a90a26e80
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 17:13:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -61,3 +61,19 @@ plt.title('Separation of Investments', fontsize=22)
 st.pyplot(fig)
 
 m = folium.Map(location=[63.391522, 96.328125], zoom_start=3)
+rel_ = folium.Choropleth(
+       geo_data = path_to_ GeoJSON_file, 
+       name = 'Отношение числа филиалов с ЕБС ко всем',
+       data = df,
+       columns=['Region', 'GRP2014'], 
+       key_on='id',
+       bins = 5,
+       fill_color='BuGn',
+       nan_fill_color='darkblue',
+       nan_fill_opacity=0.5,
+       fill_opacity=0.7,
+       line_opacity=0.2,
+       legend_name= 'Отношение числа филиалов с ЕБС ко всем',
+       highlight = True,
+       show = False
+)
