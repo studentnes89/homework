62138f210295cf97f5359f2ed9e9d96e43f5e846
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 15:29:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ Region = st.selectbox(
 df_selection = df_new[lambda x: x["Region"] == Region]
 df_selection
 
-
+fig, ax=plt.subplot()
+sns.lineplot(x=["2014", "2015", "2016"], y=["GRP2014", "GRP2015", "GRP2016"], data=df_selection)
+st.pyplot(fig)
 
 
 fig, ax = plt.subplots(figsize=(50,40), dpi= 80)    
