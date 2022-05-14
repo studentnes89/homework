3fc84b05d1c61ae6d64821cbcd68915914ef4606
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 19:51:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,16 @@ df_selection
 st.markdown("This graph shows the correlation beetween important economic growth indicators")
 fig = plt.figure(figsize=(50, 20), dpi= 80)
 sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
-plt.title('Correlogram of GDP', fontsize=60)
+plt.title('Correlogram of GRP', fontsize=60)
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 st.pyplot(fig)
 
 df_new = df[["GRP2014", "I2014", "I_prod2014", "I_inf2014"]]
 
+
 st.markdown("Let`s see closer. It`s an example of correlation beetween indicators in one year")
+
 fig = plt.figure(figsize=(50, 20), dpi= 20)
 sns.heatmap(df_new.corr(), xticklabels=df_new.corr().columns, yticklabels=df_new.corr().columns, cmap='RdYlGn', center=0, annot=True)
 plt.title('Correlogram of GRP2014', fontsize=60)
@@ -53,10 +55,11 @@ st.markdown("We distribute the regions by the smallest, medium and large GRP")
 df2=df.sort_values(by=["GRP2014"])[::10]
 fig, ax = plt.subplots(figsize=(16,10))
 ax = sns.barplot(x="Region", y="GRP2014", data=df2)
-plt.title('Distribution of regions by GRP level from the smallest to the largest', fontsize=45)
+plt.title('Distribution of regions by GRP level from the smallest to the largest', fontsize=30)
 st.pyplot(fig)
 
 st.markdown("Investments consist of Infrastructure and Production Investments. Let`s see the separation of them on the graph")
+###FROM: https://pythonru.com/biblioteki/seaborn-plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 x = df['I_prod2014']
@@ -66,10 +69,11 @@ ax.set_xlabel("Production I")
 ax.set_ylabel("Infrastructure I")
 ax.set_zlabel("Investment (all)")
 ax.scatter(x, y, z)
-plt.title('Separation of Investments', fontsize=45)
+plt.title('Separation of Investments', fontsize=20)
 st.pyplot(fig)
+###END FROM
 
-
+###Построение карты частично основывалось на примере с данного сайта https://pythonim.ru/libraries/geopandas-v-python
 st.markdown("Where is the Regions with the smallest and the biggest GRP are located?")
 st.title("Region with the smallest GRP")
 loc = 'Gorno-Altay'
@@ -100,4 +104,4 @@ st_data
 
                     
                     
-                    ###https://pythonim.ru/libraries/geopandas-v-python
+                    
