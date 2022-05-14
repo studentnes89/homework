f26b04d08649c290c22873ad2e58923480af7d64
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 11:19:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 df= pd.read_csv("data.csv")
 df
+
 large = 22; med = 16; small = 12
 params = {'axes.titlesize': large,
           'legend.fontsize': med,
@@ -18,9 +19,8 @@ params = {'axes.titlesize': large,
 plt.rcParams.update(params)
 plt.style.use('seaborn-whitegrid')
 sns.set_style("white")
-plt.figure(figsize=(50, 20), dpi= 80)
-sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
-plt.title('Correlogram of GDP', fontsize=22)
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
+
+fig, ax = plt.subplots(figsize=(50,40), dpi= 80)    
+sns.stripplot(df.GRP2015, df.I2015, size=20, ax=ax)
+plt.title('Counts Plot', fontsize=22)
 st.pyplot(fig)
