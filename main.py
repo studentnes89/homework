92fda5890b434fd63be072a0b6c6d8046653e9b2
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 11:24:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ fig, ax = plt.subplots(figsize=(50,40), dpi= 80)
 sns.stripplot(df.GRP2015, df.I2015, size=20, ax=ax)
 plt.title('Counts Plot', fontsize=22)
 st.pyplot(fig)
+
+
+plt.figure(figsize=(50, 20), dpi= 80)
+sns.heatmap(df.corr(), xticklabels=df.corr().columns, yticklabels=df.corr().columns, cmap='RdYlGn', center=0, annot=True)
+plt.title('Correlogram of GDP', fontsize=22)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.show()
