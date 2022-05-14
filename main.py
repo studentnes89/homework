392eb4d0d229320d11894f62defdd84d4138f759
studentnes89 <105539200+studentnes89@users.noreply.github.com>
2022-05-14 19:10:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,9 @@ df_new = df[["GRP2014", "I2014", "I_prod2014", "I_inf2014"]]
 
 
 fig = plt.figure(figsize=(50, 20), dpi= 200)
-sns.heatmap(df_new.corr(), xticklabels=df_new.corr().columns, yticklabels=df_new.corr().columns, cmap='RdYlGn', center=20, annot=True)
+sns.heatmap(df_new.corr(), xticklabels=df_new.corr().columns, yticklabels=df_new.corr().columns, cmap='RdYlGn', center=0, annot=True)
 plt.title('Correlogram of GDP2014', fontsize=60)
-plt.xticks(fontsize=50)
+plt.xticks(fontsize=500)
 plt.yticks(fontsize=50)
 st.pyplot(fig)
 
